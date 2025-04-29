c225e240a5436202bf3618e57a702f737277e084
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2025-04-28 23:14:33 -0700

    fix(oas): null properties
    properties cannot be null, as validated by the OAS linter.

--- aep-lib-go/pkg/openapi/openapi.go
+++ aep-lib-go/pkg/openapi/openapi.go
@@ -160,7 +160,7 @@ type Schema struct {
 	Type            string        `json:"type,omitempty"`
 	Format          string        `json:"format,omitempty"`
 	Items           *Schema       `json:"items,omitempty"`
-	Properties      Properties    `json:"properties"`
+	Properties      Properties    `json:"properties,omitempty"`
 	Ref             string        `json:"$ref,omitempty"`
 	XAEPResource    *XAEPResource `json:"x-aep-resource,omitempty"`
 	XAEPFieldNumber int           `json:"x-aep-field-number,omitempty"`
